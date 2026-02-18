1937116a314b4262ce7115878cec74db82e7d04a
“codebydhae” <dhaesha3000@gmail.com>
2026-02-18 00:36:03 -0500

    starting file structure

---
+++ components/hello.tsx
@@ -0,0 +1,10 @@
+'use client'; //activates client side component features
+
+const Hello = () => {
+    console.log('client component')
+  return (
+    <div>hello</div>
+  )
+}
+
+export default Hello
